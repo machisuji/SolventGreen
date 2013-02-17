172fbd227a2570f9c3507934cda3f332044dc182
Markus Kahl <machisuji@googlemail.com>
2013-02-17 02:04:09 +0000

    measure time for solution and number of search nodes

--- src/main/scala/solvent/green/Solver.scala
+++ src/main/scala/solvent/green/Solver.scala
@@ -19,17 +19,38 @@ trait Solver {
       (con.varNum +: con.constrainedVars).forall(solution.size >)).forall(con =>
         con.allowedFor(solution(con.varNum)).forall{ case (col, values) =>
           values contains solution(col) })
+
+  def time[R](action: => R): (R, Long) = {
+    val ns = System.nanoTime
+    val result = action
+
+    (result, System.nanoTime - ns)
+  }
 }
 
 case object BacktrackingSolver extends Solver {
+
   def solve(csp: CSP) = solve(IndexedSeq(), csp)
 
-  def solve(vars: Solution, csp: CSP): Option[Solution] = {
+  def solve(vars: Solution, csp: CSP, log: (Solution) => Unit = _ => ()): Option[Solution] = {
+    log(vars)
     if (vars.size < csp.vars.size && checkConstraints(vars, csp)) {
       csp.domains(vars.size).toIterator.map(x =>
-        solve(vars :+ x, csp)).find(_.isDefined).getOrElse(None)
+        solve(vars :+ x, csp, log)).find(_.isDefined).getOrElse(None)
     }
     else if (validate(vars, csp)) Some(vars)
     else None
+  }
+
+  def solutionAndInfo(csp: CSP, show: Boolean = false) = {
+    var calls: Int = 0
+    def log(sol: Solution) {
+      if (show) {
+        println(sol)
+      }
+      calls += 1
+    }
+    val (result, ns) = time(solve(IndexedSeq(), csp, log))
+    (result, calls, ns)
   }
 }
