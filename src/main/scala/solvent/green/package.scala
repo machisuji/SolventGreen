349ee90bb0db7cf27edf4775b733f4a772d73fa3
Markus Kahl <machisuji@googlemail.com>
2013-03-12 10:13:29 +0000

    some comments for Solution

--- src/main/scala/solvent/green/package.scala
+++ src/main/scala/solvent/green/package.scala
@@ -1,6 +1,9 @@
 package solvent
 
 package object green {
+  /**
+   * A solution maps variable indices to values.
+   */
   type Solution = Map[Int, Int]
 
   object Solution {
@@ -9,6 +12,11 @@ package object green {
   }
 
   implicit class RichSolution(val solution: Solution) extends AnyVal {
+    /**
+     * Gives a solution's values in order of their respective domains.
+     *
+     * @return A sequence of solution values.
+     */
     def toOrderedSeq: Seq[Int] = solution.toSeq.sortBy(_._1).map(_._2)
   }
 }
