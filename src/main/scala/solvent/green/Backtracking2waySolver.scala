fbcd10d2b50f875f375e616613425fbea37f68d1
Markus Kahl <machisuji@googlemail.com>
2013-03-13 16:08:16 +0000

    DRYing

--- src/main/scala/solvent/green/Backtracking2waySolver.scala
+++ src/main/scala/solvent/green/Backtracking2waySolver.scala
@@ -2,13 +2,33 @@ package solvent.green
 
 trait Backtracking2waySolver extends Solver {
   def solve(vars: Solution, csp: CSP): Option[Solution] = {
-    if (validIntermediate(vars, csp)) {
-      val next @ (i, x) = csp.selectNext(vars)
-      solve(vars + next, csp.assign(i, x)) orElse solve(vars, csp.prune(i, x)) // left orElse right
+    if (validIntermediate(vars, csp)) { // incomplete solution ok so far
+      val next @ (i, x) = csp.selectNext(vars) // i = var, x = value
+      solve(vars + next, left(i, x, csp)) orElse solve(vars, right(i, x, csp)) // branch
     }
-    else if (valid(vars, csp)) Some(vars)
-    else None
+    else if (valid(vars, csp)) Some(vars) // solution complete and valid
+    else None // no solution found
   }
+
+  /**
+   * Changes a CSP for left branching, i.e. assigns the value of the current variable.
+   *
+   * @param i current variable (number/index)
+   * @param x variable's value
+   * @param csp problem to be solved
+   * @return A new CSP whose domain for the current variable is pruned to the assigned value.
+   */
+  def left(i: Int, x: Int, csp: CSP) = csp.assign(i, x)
+
+  /**
+   * Changes a CSP for right branching, i.e. prunes the value of the current variable from its domain.
+   *
+   * @param i current variable (number/index)
+   * @param x variable's value
+   * @param csp problem to be solved
+   * @return A new CSP whose domain for the current variable is pruned by the current value.
+   */
+  def right(i: Int, x: Int, csp: CSP) = csp.prune(i, x)
 }
 
 case object Backtracking2waySolver extends Backtracking2waySolver with Logging
--- src/main/scala/solvent/green/ForwardChecking2waySolver.scala
+++ src/main/scala/solvent/green/ForwardChecking2waySolver.scala
@@ -1,15 +1,12 @@
 package solvent.green
 
-trait ForwardChecking2waySolver extends Solver with ForwardChecking {
-  def solve(vars: Solution, csp: CSP): Option[Solution] = {
-    if (validIntermediate(vars, csp)) {
-      val next @ (i, x) = csp.selectNext(vars)
-      solve(vars + next, forwardCheck(i, csp.assign(i, x))).orElse( // <left | x == n>
-        solve(vars, forwardCheck(i, csp.prune(i, x)))) // <right | x != n>
-    }
-    else if (valid(vars, csp)) Some(vars)
-    else None
-  }
+/**
+ * Works like a Backtracking2waySolver only that forward checking is applied to
+ * the CSP before branching.
+ */
+trait ForwardChecking2waySolver extends Backtracking2waySolver with ForwardChecking {
+  override def left(i: Int, x: Int, csp: CSP) = forwardCheck(i, super.left(i, x, csp))
+  override def right(i: Int, x: Int, csp: CSP) = forwardCheck(i, super.right(i, x, csp))
 }
 
 case object ForwardChecking2waySolver extends ForwardChecking2waySolver with Logging
